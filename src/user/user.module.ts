90e2aa89f1529f1a271184898cdd65029334d01a
WinterKwon <winterkwon7017@gmail.com>
2022-11-29 00:51:53 +0900

    [feat] user module 설정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
-
-@Module({})
+import { forwardRef, Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { User } from 'src/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+@Module({
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
+  controllers: [UserController],
+  providers: [UserService],
+})
 export class UserModule {}
