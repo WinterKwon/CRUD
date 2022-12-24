c44efd863f08c13031adf6ddfdbea775180a068e
WinterKwon <winterkwon7017@gmail.com>
2022-12-25 00:06:52 +0900

    [feat] poll entity 작성

--- src/entities/poll.entity.ts
+++ src/entities/poll.entity.ts
@@ -3,29 +3,46 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
-  CreateDateColumn,
-  OneToOne,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
 import { Issue } from './issue.entity';
-import { PollResult } from './pollResult.entitiy';
+
+import { User } from './user.entity';
 
 @Entity()
 export class Poll extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @CreateDateColumn({ name: 'created_at', comment: '이슈의 투표그래프 등록일' })
+  @Column({ name: 'created_at', comment: '이슈의 투표그래프 등록일' })
   pollDate: Date;
 
-  @Column()
-  pollDueDate: Date;
+  @Column({ default: 0 })
+  tiger: number;
+  @Column({ default: 0 })
+  hippo: number;
+  @Column({ default: 0 })
+  elephant: number;
+  @Column({ default: 0 })
+  dinosaur: number;
+  @Column({ default: 0 })
+  lion: number;
 
-  @Column({ default: true })
-  isPollActive: boolean;
+  @Column({ default: 0 })
+  pro: number;
 
-  @OneToOne(() => Issue, (issue) => issue.poll)
+  @Column({ default: 0 })
+  neu: number;
+
+  @Column({ default: 0 })
+  con: number;
+
+  @ManyToOne(() => Issue, (issue) => issue.polls)
+  @JoinColumn({ name: 'issue_id' })
   issue: Issue;
 
-  @OneToOne(() => PollResult)
-  pollResult: PollResult;
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  poller: User;
 }
