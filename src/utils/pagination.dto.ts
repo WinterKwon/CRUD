78e4791e0bb3a62e11f5c23991a6437915f51887
WinterKwon <winterkwon7017@gmail.com>
2022-12-31 00:37:14 +0900

    [docs] 페이지네이션 skip()매서드 작동확인 TODO 작성

--- src/utils/pagination.dto.ts
+++ src/utils/pagination.dto.ts
@@ -5,6 +5,7 @@ export class PageOptionDto {
   }
   readonly pageNum?: number;
   readonly perPage?: number;
+  //[TODO] get 으로 두지 않고 서비스층에서 호출해도 정상작동하는지 목업 데이터 반영후 확인하기
   skip(): number {
     return (this.pageNum - 1) * this.perPage;
   }
